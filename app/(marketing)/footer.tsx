7230a0b307d4017688e4372596a328375db7a862
Steven Espinal <stevenjesusespinal@gmail.com>
2024-03-11 15:57:13 -0400

    feat: Clerk Auth + Main Layout

---
+++ app/(marketing)/footer.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/hr.svg"
+            alt="Croatian"
+            className="rounded-md mr-4"
+            height={32}
+            width={40}
+          />
+          Croatian
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/es.svg"
+            alt="Spanish"
+            className="rounded-md mr-4"
+            height={32}
+            width={40}
+          />
+          Spanish
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/fr.svg"
+            alt="French"
+            className="rounded-md mr-4"
+            height={32}
+            width={40}
+          />
+          French
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/it.svg"
+            alt="Italian"
+            className="rounded-md mr-4"
+            height={32}
+            width={40}
+          />
+          Italian
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/jp.svg"
+            alt="Japanese"
+            className="rounded-md mr-4"
+            height={32}
+            width={40}
+          />
+          Japanese
+        </Button>
+      </div>
+    </footer>
+  );
+};
