d5cbf90a893287da37c58cd15cf04dbd6f8d741c
Steven Espinal <stevenjesusespinal@gmail.com>
2024-03-15 00:54:16 -0400

    feat: course progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -3,17 +3,30 @@ import { StickyWrapper } from "@/components/sticky-wrapper";
 import React from "react";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
-import { getUnits, getUserProgress } from "@/db/queries";
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+} from "@/db/queries";
 import { redirect } from "next/navigation";
 import { Unit } from "./unit";
 
 const LearnPage = async () => {
   const userProgress = await getUserProgress();
+  const courseProgress = await getCourseProgress();
+  const lessonPercentage = await getLessonPercentage();
+
   const units = await getUnits();
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
   }
+
+  if (!courseProgress) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
@@ -34,8 +47,8 @@ const LearnPage = async () => {
               description={unit.description}
               title={unit.title}
               lessons={unit.lessons}
-              activeLesson={undefined}
-              activeLessonPercentage={0}
+              activeLesson={courseProgress.activeLesson}
+              activeLessonPercentage={lessonPercentage}
             />
           </div>
         ))}
